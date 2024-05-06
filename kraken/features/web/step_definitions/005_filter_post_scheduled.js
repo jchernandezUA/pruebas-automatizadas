b63cff58d1758cc73e33552a48f1d95a82028657
miguelenruiz1 <me.ruiz42@uniandes.edu.co>
2024-05-05 23:54:15 -0500

    pruebas kraken (#19)
    * pruebas kraken
    * pruebas de cypress
    * Ajustes Then en las pruebas
    * AJuste de credenciales para cypress
    
    ---------
    Co-authored-by: miguelenruiz <miguelenruiz1@gmail.com>
    Co-authored-by: Aaron Nieves <a.nievesj@uniandes.edu.co>

---
+++ kraken/features/web/step_definitions/005_filter_post_scheduled.js
@@ -0,0 +1,40 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+When("I enter identification {string}", async function (email) {
+  let element = await this.driver.$("#identification");
+  return await element.setValue(email);
+});
+
+When("I enter password {string}", async function (password) {
+  let element = await this.driver.$("#password");
+  return await element.setValue(password);
+});
+
+When("I click next", async function () {
+  let element = await this.driver.$("#ember5");
+  return await element.click();
+});
+
+When("I select the post scheduled", async function () {
+ 
+    let dropdown = await this.driver.$(
+      ".gh-contentfilter-menu .ember-basic-dropdown-trigger"
+    );
+    await dropdown.click(); 
+    let filterOptions = await this.driver.$$(".ember-power-select-option");
+    for (let option of filterOptions) {
+      let text = await option.getText();
+      if (text.trim() === "Scheduled posts") {
+        await option.click();
+        break;
+      }
+    }
+  });
+
+  Then("I should see posts filtered by scheduled", async function () {
+    let dropdown = await this.driver.$(".gh-contentfilter-menu .ember-basic-dropdown-trigger");
+    let text = await dropdown.getText();
+    if (!text.includes("Scheduled")) {
+        throw new Error("Posts are not filtered by scheduled");
+    }
+});
