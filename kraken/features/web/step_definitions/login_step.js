af76859fde7bb7badeb18a8a2ba020c83bfca047
Jose Carlos Hernandez <jose.hernandez@line-up.com.mx>
2024-05-06 19:15:36 -0600

    - Add directory for screenshots - Add definition of screenshots on login in kraken - ignore screenshots in gitignore

--- kraken/features/web/step_definitions/login_step.js
+++ kraken/features/web/step_definitions/login_step.js
@@ -3,10 +3,11 @@ const properties = require('../../properties.json')
 const LoginPageObject = require('../support/LoginPageObject')
 
 
-Given('I login as admin in Ghost', async function () {
+Given('I login as admin in Ghost {string} {string}', async function (scenario, step) {
   let pageObject = new LoginPageObject(this.driver)
   await pageObject.goToSignIn()
   await pageObject.setEmail(properties['<GHOST_MAIL>'])
   await pageObject.setPassword(properties['<GHOST_PASSWORD>'])
   await pageObject.signIn()
+  await this.driver.saveScreenshot(`./screenshots/ss_${scenario}_${step}.png`)
 })
