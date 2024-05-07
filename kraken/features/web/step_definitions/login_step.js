aa621b4a082bad32cccd1b285b32c72189e73ecf
Jose Carlos Hernandez <jose.hernandez@line-up.com.mx>
2024-05-06 20:51:53 -0600

    add 00 on feature

--- kraken/features/web/step_definitions/login_step.js
+++ kraken/features/web/step_definitions/login_step.js
@@ -9,5 +9,5 @@ Given('I login as admin in Ghost {string} {string}', async function (scenario, s
   await pageObject.setEmail(properties['<GHOST_MAIL>'])
   await pageObject.setPassword(properties['<GHOST_PASSWORD>'])
   await pageObject.signIn()
-  await this.driver.saveScreenshot(`./screenshots/ss_${scenario}_${step}_00.png`)
+  await this.driver.saveScreenshot(`./screenshots/ss_${scenario}_${step}.png`)
 })
