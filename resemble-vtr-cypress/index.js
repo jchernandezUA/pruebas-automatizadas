8195de6ba945c8b59f9dc80b803766f97becd98d
Jose Carlos Hernandez <jose.hernandez@line-up.com.mx>
2024-05-08 22:21:32 -0600

    Add output report

--- resemble-vtr-cypress/index.js
+++ resemble-vtr-cypress/index.js
@@ -24,45 +24,8 @@ function runCypressTests() {
 const { viewportHeight, viewportWidth, browsers, options } = config;
 
 async function executeTest(){
-  let resultInfo = {}
-    /*if(browsers.length === 0){
-      return;
-    }
-    let datetime = new Date().toISOString().replace(/:/g,".");
-    for(b of browsers){
-        if(!b in ['chromium', 'webkit', 'firefox']){
-            return;
-        }
-        if (!fs.existsSync(`./results/${datetime}`)){
-            fs.mkdirSync(`./results/${datetime}`, { recursive: true });
-        }
-        //Launch the current browser context
-        const browser = await playwright[b].launch({headless: true, viewport: {width:viewportWidth, height:viewportHeight}});
-        const context = await browser.newContext();
-        const page = await context.newPage(); 
-        await page.goto(config.url);
-        await page.screenshot({ path: `./results/${datetime}/before-${b}.png` });
-        await page.click('#generate');
-        await page.screenshot({ path: `./results/${datetime}/after-${b}.png` });
-        await browser.close();
-        
-        const data = await compareImages(
-            fs.readFileSync(`./results/${datetime}/before-${b}.png`),
-            fs.readFileSync(`./results/${datetime}/after-${b}.png`),
-            options
-        );
-        resultInfo[b] = {
-            isSameDimensions: data.isSameDimensions,
-            dimensionDifference: data.dimensionDifference,
-            rawMisMatchPercentage: data.rawMisMatchPercentage,
-            misMatchPercentage: data.misMatchPercentage,
-            diffBounds: data.diffBounds,
-            analysisTime: data.analysisTime
-        }
-        fs.writeFileSync(`./results/${datetime}/compare-${b}.png`, data.getBuffer());
-
-    }
-*/
+  let resultInfo = {
+  }
   console.log("Running cypress test")
   await runCypressTests()
   .then((stdout) => {
@@ -93,11 +56,13 @@ async function executeTest(){
     package.push(compareFile)
     resultFies.push(package)
   }
+  let reportFilenName = `./results/report.html`
   fs.writeFileSync(`./results/report.html`, createReport(datetime, resultFies));
   fs.copyFileSync('./index.css', `./results/index.css`);
+  resultInfo['report'] =  reportFilenName
   console.log('------------------------------------------------------------------------------------')
   console.log("Execution finished. Check the report under the results folder")
-  return {};  
+  return resultInfo;  
 }
 
 
