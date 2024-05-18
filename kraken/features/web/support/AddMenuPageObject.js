c21375c41ce8cb13bb0dfce0310087ab1be7298b
Aaron Nieves <a.nievesj@uniandes.edu.co>
2024-05-18 02:42:50 -0500

    Move dashboard steps to their corresponding page object

--- kraken/features/web/support/AddMenuPageObject.js
+++ kraken/features/web/support/AddMenuPageObject.js
@@ -7,12 +7,6 @@ class AddMenuPageObject extends BasePageObject {
     super(driver);
   }
 
-  async clickSettingsIcon() {
-    let element = await this.driver.$('/html/body/div[2]/div/nav[1]/div/section/div[2]/div/div/div[2]/a');
-    await element.waitForClickable();
-    return await element.click();
-  }
-
   async clickNavigationCustomizeButton() {
     let element = await this.driver.$('/html/body/div[2]/div/main/div[1]/div/div/div[3]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button');
     await element.waitForClickable();
@@ -37,10 +31,6 @@ class AddMenuPageObject extends BasePageObject {
     return await element.click();
   }
 
-  async goToHomepage() {
-    await this.driver.url(properties['<URL_HOME>']);
-  }
-
   async seeTheNewItemMenu(expectedText) {
     const expect = (await import('expect-webdriverio')).expect;
     let label = await this.driver.$('/html/body/div[1]/header/div/nav/ul/li[3]/a');
@@ -49,16 +39,11 @@ class AddMenuPageObject extends BasePageObject {
     await expect(label).toHaveHref(`${properties['<URL_HOME>']}/`);
   }
 
-  async goToAdminPage() {
-    await this.driver.url(`${properties['<URL>']}`);
-  }
-
   async deleteItemMenu() {
     let el = await this.driver.$('/html/body/div[2]/div/main/div[1]/div/div/div[4]/section/div[1]/div/section/div[2]/div/div/div[1]/div/div[3]/div/button');
     await el.waitForClickable();
     return await el.click();
   }
-
 }
 
 module.exports = AddMenuPageObject;
