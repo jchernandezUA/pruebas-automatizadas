f6992a86a88cbea163ebcdfc3248ca75a7b9485c
Aaron Nieves <a.nievesj@uniandes.edu.co>
2024-05-19 15:28:06 -0500

    Add 18 site private scenarios

--- cypress/cypress/support/SitePrivatePageObject.js
+++ cypress/cypress/support/SitePrivatePageObject.js
@@ -1,9 +1,52 @@
 import BasePageObject from "../support/BasePageObject";
+import passwords from '../fixtures/aanieves/password.json';
+const { faker } = require('@faker-js/faker');
 
 class SitePrivatePageObject extends BasePageObject {
 
   screenshot(number) {
     cy.screenshot('ss_site_private_0' + number);
+  }
+
+  getRandomPassword() {
+    const randomNumber = Math.floor(Math.random() * passwords.length);
+    return passwords[randomNumber].password;
+  }
+
+  getRandomPasswordFromAPI() {
+    return cy.request({
+      method: 'GET',
+      url: 'https://my.api.mockaroo.com/password.json?key=6d151b10'
+    }).then(response => {
+      const passwordArray = response.body;
+      const randomNumber = Math.floor(Math.random() * passwordArray.length);
+      return passwordArray[randomNumber].password;
+    });
+  }
+
+  setRandomPrivatePassword() {
+    const password = this.getRandomPassword();
+    cy.get('input[placeholder="Enter password"]')
+      .should('exist')
+      .clear()
+      .type(password);
+  }
+
+  setRandomPrivatePasswordFromAPI() {
+    this.getRandomPasswordFromAPI().then(password => {
+      cy.get('input[placeholder="Enter password"]')
+        .should('exist')
+        .clear()
+        .type(password);
+    });
+  }
+
+  setFakerPrivatePassword(){
+    let password = faker.location.countryCode();
+    cy.get('input[placeholder="Enter password"]')
+      .should('exist')
+      .clear()
+      .type(password);
   }
 
   clickSitePrivateButton() {
@@ -37,7 +80,6 @@ class SitePrivatePageObject extends BasePageObject {
       .invoke('text')
       .should('eq', 'This site is private.');
   }
-
 }
 
 export default new SitePrivatePageObject()
